995fa45c7b0fc130f35fd6e94d5c524385be1470
Marco Montanari <sirmmo@gmail.com>
2011-05-01 05:19:43 -0700

    corrected syntax

--- server/models.py
+++ server/models.py
@@ -20,21 +20,21 @@ from django.db import models
 #These are some dumb mappings of the original OpenID store tables as used by the SQLStore implemenation(s).
 #They're used by "DjangoidStore"
 class OidStoreNonce(models.Model):
-        nonce = models.CharField(maxlength = 8, primary_key = True)
+        nonce = models.CharField(max_length = 8, primary_key = True)
         expires = models.IntegerField()
 
 class OidStoreAssociation(models.Model):
         server_url = models.TextField()
-        handle = models.CharField(maxlength = 255)
+        handle = models.CharField(max_length = 255)
         secret = models.TextField()
         issued = models.IntegerField()
         lifetime = models.IntegerField()
-        assoc_type = models.CharField(maxlength = 64)
+        assoc_type = models.CharField(max_length = 64)
 
         class Meta:
                 #Django got no multi-column primary keys
                 unique_together = (("server_url", "handle"),)
 
 class OidStoreSetting(models.Model):
-        setting = models.CharField(maxlength = 128, primary_key = True)
+        setting = models.CharField(max_length = 128, primary_key = True)
         value = models.TextField()

4f658aa75b65beb68b2d5d2a5e41a1136b4d4f45
Marco Montanari <sirmmo@gmail.com>
2011-05-04 04:21:25 -0700

    no more test id

--- urls.py
+++ urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 
 admin.autodiscover()
 urlpatterns = patterns('',
-    url(r'^testid/$', 'users.views.testid'),
     url(r'^yadis/$', 'server.views.serveryadis'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login', {"template_name": "users/login.html"}),
